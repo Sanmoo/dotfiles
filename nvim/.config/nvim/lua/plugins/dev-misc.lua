aafd908c11dc3cee553f686666a44036e0f5ba7e
Samuel Santos <sanmoo.cs@gmail.com>
2025-11-10 17:19:12 -0300

    add more configs

--- nvim/.config/nvim/lua/plugins/dev-misc.lua
+++ nvim/.config/nvim/lua/plugins/dev-misc.lua
@@ -1,9 +1,26 @@
--- Enable TypeSpec
-vim.lsp.enable("tsp_server")
-
 return {
-  "vinnymeller/swagger-preview.nvim",
-  cmd = { "SwaggerPreview", "SwaggerPreviewStop", "SwaggerPreviewToggle" },
-  build = "npm i",
-  config = true,
+  {
+    "vinnymeller/swagger-preview.nvim",
+    cmd = { "SwaggerPreview", "SwaggerPreviewStop", "SwaggerPreviewToggle" },
+    build = "npm i",
+    config = true,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        kotlin_language_server = {
+          enabled = false
+        },
+        -- Enable Kotlin Language Server
+        -- Not working for me
+        -- kotlin_lsp = {},
+        -- Not working for me either
+        -- kotlin_language_server = {},
+
+        -- Enable TypeSpec Server
+        tsp_server = {},
+      }
+    }
+  }
 }
