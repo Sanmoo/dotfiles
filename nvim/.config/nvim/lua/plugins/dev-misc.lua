d7f0feb7c07a483fd61e608177af6c29c48b92d2
Samuel Santos <sanmoo.cs@gmail.com>
2025-11-11 07:53:24 -0300

    more updates

--- nvim/.config/nvim/lua/plugins/dev-misc.lua
+++ nvim/.config/nvim/lua/plugins/dev-misc.lua
@@ -10,17 +10,36 @@ return {
     opts = {
       servers = {
         kotlin_language_server = {
-          enabled = false
+          -- enabled = false
         },
         -- Enable Kotlin Language Server
+        --
         -- Not working for me
-        -- kotlin_lsp = {},
-        -- Not working for me either
-        -- kotlin_language_server = {},
+        kotlin_lsp = {
+          enabled = false
+        },
 
         -- Enable TypeSpec Server
         tsp_server = {},
       }
     }
+  },
+  -- SQL stuff
+  {
+    'kristijanhusak/vim-dadbod-ui',
+    dependencies = {
+      { 'tpope/vim-dadbod', lazy = true },
+      { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql', 'oracle', 'sqlite' }, lazy = true }, -- Optional
+    },
+    cmd = {
+      'DBUI',
+      'DBUIToggle',
+      'DBUIAddConnection',
+      'DBUIFindBuffer',
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
   }
 }
