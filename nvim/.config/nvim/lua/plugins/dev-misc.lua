be0d70d89225b6516c32933ee450264b32876433
Samuel Santos <sanmoo.cs@gmail.com>
2025-11-12 17:53:40 -0300

    add some niceties

--- nvim/.config/nvim/lua/plugins/dev-misc.lua
+++ nvim/.config/nvim/lua/plugins/dev-misc.lua
@@ -23,6 +23,11 @@ return {
         tsp_server = {},
       },
     },
+    { "weilbith/neotest-gradle" },
+    {
+      "nvim-neotest/neotest",
+      opts = { adapters = { "neotest-gradle" } },
+    },
 
     -- SQL stuff
     -- {
@@ -42,22 +47,6 @@ return {
     --   opts = {
     --     enabled = true,
     --   },
-    -- },
-    -- {
-    --   "kndndrj/nvim-dbee",
-    --   dependencies = {
-    --     "MunifTanjim/nui.nvim",
-    --   },
-    --   build = function()
-    --     -- Install tries to automatically detect the install method.
-    --     -- if it fails, try calling it with one of these parameters:
-    --     --    "curl", "wget", "bitsadmin", "go"
-    --     require("dbee").install()
-    --   end,
-    --   config = function()
-    --     require("dbee").setup(--[[optional config]])
-    --   end,
-    --   opts = { enabled = false },
     -- },
   },
 }
