d89596d811edca1aae459926a6d835d2149c76c2
Samuel Santos <sanmoo.cs@gmail.com>
2025-12-09 09:55:05 -0300

    update dotfiles

--- nvim/.config/nvim/lua/plugins/navigation.lua
+++ nvim/.config/nvim/lua/plugins/navigation.lua
@@ -40,5 +40,14 @@ return {
       },
     },
   },
+  {
+    "s1n7ax/nvim-window-picker",
+    name = "window-picker",
+    event = "VeryLazy",
+    version = "2.*",
+    config = function()
+      require("window-picker").setup()
+    end,
+  },
   -- Other UI plugins...
 }
